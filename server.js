5b2428b51d93c4fcfa5508f1e7aff3704d65e20f
Abdelhamid Kerkeni <abkerkeni@gmail.com>
2017-11-19 20:01:50 +0100

    update server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 
 var AlexaAppServer = require( 'alexa-app-server' );
 
+
+var CheckServer = function() {
+   console.log("Check server working");
+
+    });
+}
+
 var server = new AlexaAppServer( {
 	httpsEnabled: false,
 	port: process.env.PORT || 80,
@@ -9,5 +16,7 @@ var server = new AlexaAppServer( {
 	log: true
 	
 } );
+
+CheckServer();
 
 server.start();
